bf5b6d8155dd3a514de9409ffec6e3a40fb387d4
rsenecal <roodysenecal@gmail.com>
2022-10-23 23:47:08 -0400

    add classes engineer, intern, manager

---
+++ index.js
@@ -0,0 +1,6 @@
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager')
+
+let myManager = new Manager("Roody", 123455,"roodysenecal@gmail.com", "215-909-8767");
+
+console.log(myManager)
